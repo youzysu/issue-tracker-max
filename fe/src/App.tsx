01010e022f397d84750a9c9852b671033874db5b
Jisoo Yoo <youzysu@gmail.com>
2023-07-25 00:02:07 +0900

    #7 refactor: 로고 컴포넌트 props 구조 수정

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,4 +1,4 @@
-import { Logo } from "@components/common/Logo";
+import Logo from "@components/common/Logo";
 import styled, { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/GlobalStyle";
 import designSystem from "styles/designSystem";
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <ThemeProvider theme={designSystem}>
       <GlobalStyle />
-      <Logo width={200} height={40}></Logo>
+      <Logo size="large" />
       <H1>적용 되니?</H1>
     </ThemeProvider>
   );
