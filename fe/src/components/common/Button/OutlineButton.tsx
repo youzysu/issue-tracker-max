693db65d043fcfd128c7b24960a33cac20d9f457
Jisoo Yoo <youzysu@gmail.com>
2023-07-26 01:06:21 +0900

    #10 refactor: 컨벤션 추가 적용

--- fe/src/components/common/Button/OutlineButton.tsx
+++ fe/src/components/common/Button/OutlineButton.tsx
@@ -10,8 +10,8 @@ export default function OutlineButton(props: ButtonProps) {
 const StyledOutlineButton = styled(BaseButton)<ButtonProps>`
   width: ${({ size }) => size && SIZE[size].width};
   height: ${({ size }) => size && SIZE[size].height};
-  border-radius: ${({ theme, size }) =>
-    size === "L" ? theme.radius.l : theme.radius.m};
+  border-radius: ${({ theme: { radius }, size }) =>
+    size === "L" ? radius.l : radius.m};
 
   border: ${({ theme: { border, brand } }) =>
     `${border.default} ${brand.border.default}`};
