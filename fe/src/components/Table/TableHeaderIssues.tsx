fba1a66e7dc7101c2a9bf58766e7244d5fd98cfd
Jisoo Yoo <youzysu@gmail.com>
2023-07-31 16:13:21 +0900

    #10 refactor: TabBar 재사용을 위한 리팩토링
    - props: left, right, borderType

--- fe/src/components/Table/TableHeaderIssues.tsx
+++ fe/src/components/Table/TableHeaderIssues.tsx
@@ -1,27 +1,29 @@
 import alertIcon from "@assets/icon/alertCircle.svg";
 import archiveIcon from "@assets/icon/archive.svg";
 import DropdownIndicator from "@components/Dropdown/DropdownIndicator";
-import Button from "@components/common/Button";
 import InputCheckbox from "@components/common/Input/InputCheckbox";
+import TabBar from "@components/common/TabBar";
 import { styled } from "styled-components";
 import TableHeader from "./TableHeader";
 
 export default function TableHeaderIssues() {
+  const tabBarLeftInfo = { name: "열린 이슈", count: 2, iconSrc: alertIcon };
+  const tabBarRightInfo = {
+    name: "닫힌 이슈",
+    count: 0,
+    iconSrc: archiveIcon,
+  };
+
   return (
     <TableHeader>
       <TableHeaderContents>
         <div className="left-wrapper">
           <InputCheckbox />
-
-          {/* TODO: TabBar로 변경 */}
-          <Button size="M" variant="ghost">
-            <img src={alertIcon} alt="열린 이슈" />
-            열린 이슈 (2)
-          </Button>
-          <Button size="M" variant="ghost">
-            <img src={archiveIcon} alt="닫힌 이슈" />
-            닫힌 이슈 (0)
-          </Button>
+          <TabBar
+            left={tabBarLeftInfo}
+            right={tabBarRightInfo}
+            borderType="none"
+          />
         </div>
 
         <div className="right-wrapper">
