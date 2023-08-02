b3694b116bd4af9ff9417f06a9ce6efceca31063
Jisoo Yoo <youzysu@gmail.com>
2023-08-02 13:38:04 +0900

    #51 refactor: input focus 상태 제거 & css :focus로 스타일 구현

--- fe/src/components/common/Label.tsx
+++ fe/src/components/common/Label.tsx
@@ -4,4 +4,5 @@ export const Label = styled.label`
   display: flex;
   align-items: center;
   font: ${({ theme: { font } }) => font.displayMD12};
+  color: ${({ theme: { neutral } }) => neutral.text.weak};
 `;
--- fe/src/components/common/TextArea.tsx
+++ fe/src/components/common/TextArea.tsx
@@ -1,5 +1,4 @@
 import paperClipIcon from "@assets/icon/paperclip.svg";
-import { useState } from "react";
 import styled from "styled-components";
 import { Label } from "./Label";
 
@@ -8,39 +7,26 @@ export default function TextArea({
   value,
   ...props
 }: React.TextareaHTMLAttributes<HTMLTextAreaElement>) {
-  const [isFocused, setIsFocused] = useState(false);
-
-  const textAreaState = isFocused ? "active" : "enabled";
-  const typingState = isFocused ? "onTyping" : "typed";
-
   return (
-    <TextAreaContainer $state={textAreaState}>
+    <TextAreaContainer>
       {value && <Label htmlFor={name}>{props.placeholder}</Label>}
-      <StyledTextArea
-        id={name}
-        $typingState={typingState}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-        {...props}
-      />
+      <StyledTextArea id={name} {...props} />
       <FileUploadButton>
-        <label className="file-upload-label" htmlFor="file-upload-input">
+        <Label className="file-upload-label" htmlFor="file-upload-input">
           <img
             className="file-upload-icon"
             src={paperClipIcon}
             alt="파일 첨부 아이콘"
           />
           파일 첨부하기
-        </label>
+        </Label>
         <input type="file" id="file-upload-input" />
       </FileUploadButton>
     </TextAreaContainer>
   );
 }
 
-const TextAreaContainer = styled.div<{
-  $state: "enabled" | "active" | "disabled";
-}>`
+const TextAreaContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -49,47 +35,35 @@ const TextAreaContainer = styled.div<{
   padding: 16px;
   gap: 8px;
   color: ${({ theme: { neutral } }) => neutral.text.weak};
-  border: ${({ $state, theme: { neutral, border } }) => {
-    const type = {
-      active: `${border.default} ${neutral.border.defaultActive}`,
-      disabled: "none",
-      enabled: "none",
-    };
-    return type[$state];
-  }};
   border-radius: ${({ theme: { radius } }) => `${radius.l}`};
-  background-color: ${({ $state, theme: { neutral } }) => {
-    const type = {
-      active: neutral.surface.strong,
-      disabled: neutral.surface.bold,
-      enabled: neutral.surface.bold,
-    };
-    return type[$state];
-  }};
-  opacity: ${({ $state, theme: { opacity } }) =>
-    $state === "disabled" && opacity.disabled};
+  background-color: ${({ theme: { neutral } }) => neutral.surface.bold};
+  &:focus-within {
+    border: ${({ theme: { neutral, border } }) =>
+      `${border.default} ${neutral.border.defaultActive}`};
+    background-color: ${({ theme: { neutral } }) => neutral.surface.strong};
+  }
+  &:disabled {
+    opacity: ${({ theme: { opacity } }) => opacity.disabled};
+  }
 `;
 
-const StyledTextArea = styled.textarea<{
-  $typingState: "onTyping" | "typed";
-}>`
+const StyledTextArea = styled.textarea`
   display: flex;
   width: 100%;
   height: 100%;
+  color: ${({ theme: { neutral } }) => neutral.text.default};
   font: ${({ theme: { font } }) => font.displayMD16};
   caret-color: ${({ theme: { palette } }) => palette.blue};
   &::placeholder {
     color: ${({ theme: { neutral } }) => neutral.text.weak};
   }
+  &:focus {
+    color: ${({ theme: { neutral } }) => neutral.text.strong};
+  }
 `;
 
 const FileUploadButton = styled.div`
-  font: ${({ theme: { font } }) => font.displayMD12};
-  color: ${({ theme: { neutral } }) => neutral.text.default};
-
   .file-upload-label {
-    display: flex;
-    align-items: center;
     gap: 4px;
   }
 
